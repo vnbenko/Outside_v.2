5f9a2540cdee49d87123a7a5ddeae822bba48a51
Vladimir Benko <vnbenko@gmail.com>
2022-12-24 10:41:23 +0300

    Initial Commit

---
+++ Outside/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outside
+//
+//  Created by Vladimir Benko on 24.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
